9d02d0f4c716ee721c3a3f985eacd76aaa208ee7
lingbopro <ilovemc@zohomail.cn>
2024-11-10 10:43:25 +0800

    Basic Template
    The modified basic fabric mod template

---
+++ src/main/java/me/lingbopro/rejoinnofalldamageisback/Rejoinnofalldamageisback.java
@@ -0,0 +1,10 @@
+package me.lingbopro.rejoinnofalldamageisback;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Rejoinnofalldamageisback implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/me/lingbopro/rejoinnofalldamageisback/client/RejoinnofalldamageisbackClient.java
@@ -0,0 +1,10 @@
+package me.lingbopro.rejoinnofalldamageisback.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RejoinnofalldamageisbackClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
